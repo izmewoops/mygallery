158b1d96ec7bc619330ff142a319585697576f9f
izmewoops <74931976+izmewoops@users.noreply.github.com>
2020-11-24 23:31:55 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html> 
+<html>
+<style>
+img {
+   display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  padding: 5px;
+   border: 1px solid black;
+}
+
+</style>
+<body>
+<center> 
+<h1> MY GALLERY </h1>
+</center>
+<center>
+<h1>
+<a style="text-decoration:none" href="index.php" align="middle"> ELEMENTARY DAYS </a>  |
+<a style="text-decoration:none" href="highshool.html" align="middle"> HIGHSCHOOL DAYS </a>  |
+<a style="text-decoration:none" href="college.html" align="middle"> COLLEGE DAYS </a>  |
+</h1>
+</center>
+<div class="img">
+<a href="elem1.jpg"><img src="elem1.jpg"/></a>
+<a href="elem2.jpg"><img src="elem2.jpg"/></a>
+<a href="elem3.jpg"><img src="elem3.jpg"/></a>
+<a href="elem4.jpg"><img src="elem4.jpg"/></a>
+<a href="elem5.jpg"><img src="elem5.jpg"/></a>
+</div>
